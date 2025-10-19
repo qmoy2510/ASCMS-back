500c6d0bc4b67ae256d079bc0c096c851d95cf20
Shuung <qmoy2510@gmail.com>
2025-10-20 08:49:18 +0900

    cors 진짜 해결

--- afterSchool/src/main/java/com/example/afterSchool/config/WebConfig.java
+++ afterSchool/src/main/java/com/example/afterSchool/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:8088")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
